de37bcbc788bdf3a35ac8a105fa22d19bb1d8acc
sudarat29 <636051200237@mail.rmutk.ac.th>
2022-02-18 20:02:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/assignment6_1/MainActivity2.kt
@@ -0,0 +1,19 @@
+package com.example.assignment6_1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        show.text = intent.getStringExtra(result)
+        button .setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
